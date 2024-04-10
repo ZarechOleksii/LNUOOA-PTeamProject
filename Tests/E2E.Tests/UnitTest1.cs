f188f4d210c05ec7db06e92ad1396ac793b05337
ZarechOleksii <misterz06022002@gmail.com>
2024-04-11 00:39:23 +0300

    added test projects

---
+++ Tests/E2E.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace E2E.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Tests/ProcessorWebApi.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ProcessorWebApi.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
